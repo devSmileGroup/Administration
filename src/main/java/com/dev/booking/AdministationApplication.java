374f9a2dd6166349f4d7a2f4bb39740ec5c055f9
kolya-mazurok2 <kolya.mazurok2@gmail.com>
2018-06-07 17:17:51 +0300

    all

---
+++ src/main/java/com/dev/booking/AdministationApplication.java
@@ -0,0 +1,12 @@
+package com.dev.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministationApplication.class, args);
+	}
+}
